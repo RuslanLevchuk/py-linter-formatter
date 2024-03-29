d10d4738fd06e4a8c43ffb55bbeea67674bd9e55
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-03-30 01:26:17 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,46 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors":
+            [
+                {
+                    "line": error["line_number"],
+                    "column": error["column_number"],
+                    "message": error["text"],
+                    "name": error["code"],
+                    "source": "flake8"
+                }
+                for error in errors if error["filename"] == file_path
+            ],
+        "path": file_path,
+        "status": "failed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        {
+            "errors":
+                [
+                    {
+                        "line": source_value["line_number"],
+                        "column": source_value["column_number"],
+                        "message": source_value["text"],
+                        "name": source_value["code"],
+                        "source": "flake8"
+                    }
+                    for source_value in source_values
+                ],
+            "path": source_key,
+            "status": "passed" if len(source_values) == 0 else "failed"
+        } for source_key, source_values in linter_report.items()
+    ]
